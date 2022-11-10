a6d0b478e395b431b730f96a7993c81656bd8e73
Tinotenda Nyashanau <tinotendanyash@gmail.com>
2022-11-10 01:22:42 +0100

    loading file

--- voiceRecognitionApp/Form1.Designer.cs
+++ voiceRecognitionApp/Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstCommands = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstCommands
+            // 
+            this.lstCommands.BackColor = System.Drawing.SystemColors.Desktop;
+            this.lstCommands.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lstCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstCommands.FormattingEnabled = true;
+            this.lstCommands.ItemHeight = 16;
+            this.lstCommands.Location = new System.Drawing.Point(0, 0);
+            this.lstCommands.Name = "lstCommands";
+            this.lstCommands.Size = new System.Drawing.Size(800, 450);
+            this.lstCommands.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstCommands);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstCommands;
     }
 }
 
--- voiceRecognitionApp/Form1.cs
+++ voiceRecognitionApp/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
 
 namespace voiceRecognitionApp
 {
     public partial class Form1 : Form
     {
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Tino = new SpeechSynthesizer();   
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int Rectimeout = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines("")))));
         }
     }
 }
